9dea193250be650140d9e5a4de0054336d0ee54e
Oussama Jarrousse <oussama@jarrousse.org>
2022-03-16 11:40:32 +0100

    addde 1 test; moved BrowserRouter to App()

--- consistent/src/App.js
+++ consistent/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import ProjectPage from './components/pages/ProjectPage';
 import NewProjectPage from './components/pages/NewProjectPage'
@@ -10,18 +10,20 @@ import {ProjectContextProvider} from './components/context/ProjectContext'
 function App() {
 
   return (
-    <>      
-      <Layout>
-        <ProjectContextProvider>
-          <Switch>
-            <Route path="/" exact component={HomePage} />
-            <Route path="/projects/new" exact component={NewProjectPage} />
-            <Route path="/projects/:projectId" exact component={ProjectPage} />
-            {/* <Route path="/projects/" exact component={ProjectsPage} /> */}
-            <Route path="/about" exact component={AboutPage} />
-          </Switch>
-        </ProjectContextProvider>
-      </Layout>
+    <>
+      <BrowserRouter>
+        <Layout>
+          <ProjectContextProvider>
+            <Switch>
+              <Route path="/" exact component={HomePage} />
+              <Route path="/projects/new" exact component={NewProjectPage} />
+              <Route path="/projects/:projectId" exact component={ProjectPage} />
+              {/* <Route path="/projects/" exact component={ProjectsPage} /> */}
+              <Route path="/about" exact component={AboutPage} />
+            </Switch>
+          </ProjectContextProvider>
+        </Layout>
+      </BrowserRouter>
     </>
   );
 }
