8941bf48f44feafb3f0f9e2a19fa34b64c1c5a9a
Oussama Jarrousse <oussama@jarrousse.org>
2022-03-31 04:55:03 +0200

    cleaning up the Navigation component; moved LoadFile to a separate component

--- consistent/src/App.js
+++ consistent/src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import ProjectPage from './components/pages/ProjectPage';
-import NewProjectPage from './components/pages/NewProjectPage'
-import LoadFixturessPage from './components/pages/LoadFixturesPage'
+import NewProjectPage from './components/pages/NewProjectPage';
+import LoadFixturesPage from './components/pages/LoadFixturesPage';
 import HomePage from './components/pages/HomePage';
 import AboutPage from './components/pages/AboutPage';
 import Layout from './components/layout/Layout';
@@ -18,18 +18,18 @@ function App() {
       <BrowserRouter>
         
         <ProjectContextProvider>       
-           <Layout>
-            <FilterContextProvider>          
-              <Switch>
-                  <Route path="/" exact component={HomePage} />
-                  <Route path="/projects/_new" exact component={NewProjectPage} />
-                  <Route path="/projects/_example" exact component={LoadFixturessPage} />
-                  <Route path="/projects/:projectId" exact component={ProjectPage} />
-                  {/* <Route path="/projects/" exact component={ProjectsPage} /> */}
-                  <Route path="/about" exact component={AboutPage} />
-              </Switch>
-              </FilterContextProvider>     
-             </Layout>
+            <FilterContextProvider>
+              <Layout>                          
+                  <Switch>
+                      <Route path="/" exact component={HomePage} />
+                      <Route path="/projects/_new" exact component={NewProjectPage} />
+                      <Route path="/projects/_example" exact component={LoadFixturesPage} />
+                      <Route path="/projects/:projectId" exact component={ProjectPage} />
+                      {/* <Route path="/projects/" exact component={ProjectsPage} /> */}
+                      <Route path="/about" exact component={AboutPage} />
+                  </Switch>
+                </Layout>
+              </FilterContextProvider> 
             </ProjectContextProvider>                
        
       </BrowserRouter>
